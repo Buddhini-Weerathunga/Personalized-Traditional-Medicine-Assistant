f651addc19437c57c6a50ce2fda9173a6be868c4
Suhana Yaseer <127375037+IT22885432@users.noreply.github.com>
2025-11-14 20:48:38 +0530

    Add PrakritiAnalysisPage component to integrate WebcamCapture

---
+++ frontend/src/pages/PrakritiAnalysisPage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import WebcamCapture from "../components/prakriti/WebcamCapture";
+
+export default function PrakritiAnalysisPage() {
+  return (
+    <div className="max-w-5xl mx-auto">
+      <WebcamCapture />
+    </div>
+  );
+}
