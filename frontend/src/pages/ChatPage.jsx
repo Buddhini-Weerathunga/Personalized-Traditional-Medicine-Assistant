6ee6f1863addc9ff7c68f8f8116bacabd6fa9ee5
Suhana Yaseer <127375037+IT22885432@users.noreply.github.com>
2025-11-14 20:47:35 +0530

    Add ChatPage component for chat interface

---
+++ frontend/src/pages/ChatPage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ChatWindow from "../components/chat/ChatWindow";
+
+export default function ChatPage() {
+  return (
+    <div className="max-w-5xl mx-auto">
+      <ChatWindow />
+    </div>
+  );
+}
