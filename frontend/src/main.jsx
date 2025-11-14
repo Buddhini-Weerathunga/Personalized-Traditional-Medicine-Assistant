7b4c0d2b48fdcd32cb6628188518317459d0387a
Suhana Yaseer <127375037+IT22885432@users.noreply.github.com>
2025-11-14 20:45:31 +0530

    Refactor main component to include routing and layout structure with ChatProvider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,42 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./styles/globals.css";
+import "./styles/chat.css";
+import "./styles/prakriti.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import { ChatProvider } from "./context/ChatContext";
+import Navbar from "./components/layout/Navbar";
+import Sidebar from "./components/layout/Sidebar";
+
+import HomePage from "./pages/HomePage";
+import ChatPage from "./pages/ChatPage";
+import PrakritiAnalysisPage from "./pages/PrakritiAnalysisPage";
+
+function AppShell() {
+  return (
+    <BrowserRouter>
+      <ChatProvider>
+        <div className="app-shell">
+          <Sidebar />
+          <div className="flex-1 flex flex-col">
+            <Navbar />
+            <main className="main-content">
+              <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/chat" element={<ChatPage />} />
+                <Route path="/prakriti" element={<PrakritiAnalysisPage />} />
+              </Routes>
+            </main>
+          </div>
+        </div>
+      </ChatProvider>
+    </BrowserRouter>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <AppShell />
+  </React.StrictMode>
+);
