f6db559ab96d509bbc70b38bfc766fcd4377f6eb
Suhana Yaseer <127375037+IT22885432@users.noreply.github.com>
2025-11-16 23:06:31 +0530

    chore: setup React app entry point with BrowserRouter and Tailwind

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,42 +1,14 @@
+// frontend/src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./styles/globals.css";
-import "./styles/chat.css";
-import "./styles/prakriti.css";
-
-import { ChatProvider } from "./context/ChatContext";
-import Navbar from "./components/layout/Navbar";
-import Sidebar from "./components/layout/Sidebar";
-
-import HomePage from "./pages/HomePage";
-import ChatPage from "./pages/ChatPage";
-import PrakritiAnalysisPage from "./pages/PrakritiAnalysisPage";
-
-function AppShell() {
-  return (
-    <BrowserRouter>
-      <ChatProvider>
-        <div className="app-shell">
-          <Sidebar />
-          <div className="flex-1 flex flex-col">
-            <Navbar />
-            <main className="main-content">
-              <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/chat" element={<ChatPage />} />
-                <Route path="/prakriti" element={<PrakritiAnalysisPage />} />
-              </Routes>
-            </main>
-          </div>
-        </div>
-      </ChatProvider>
-    </BrowserRouter>
-  );
-}
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import "./index.css"; // Tailwind styles
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AppShell />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
