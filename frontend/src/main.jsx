408a6b4d449a0b2664da1c355fdb2f0aed0b8b78
Suhana Yaseer <127375037+IT22885432@users.noreply.github.com>
2025-11-19 12:31:54 +0530

    Refactor main.jsx to wrap App with BrowserRouter and ChatProvider for routing and context setup

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,11 +4,14 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css"; // Tailwind styles
+import { ChatProvider } from "./context/ChatContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ChatProvider>
+        <App />
+      </ChatProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
