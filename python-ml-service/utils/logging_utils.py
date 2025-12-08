690ec16b1860540dbdaa2df63c15ef081a538923
Suhana Yaseer <127375037+IT22885432@users.noreply.github.com>
2025-12-08 18:28:06 +0530

    add logging_utils

---
+++ python-ml-service/utils/logging_utils.py
@@ -0,0 +1,14 @@
+import logging
+
+def get_logger(name: str) -> logging.Logger:
+    logger = logging.getLogger(name)
+    if not logger.handlers:
+        logger.setLevel(logging.INFO)
+        ch = logging.StreamHandler()
+        formatter = logging.Formatter(
+            "[%(asctime)s] [%(name)s] [%(levelname)s] %(message)s",
+            datefmt="%Y-%m-%d %H:%M:%S",
+        )
+        ch.setFormatter(formatter)
+        logger.addHandler(ch)
+    return logger
