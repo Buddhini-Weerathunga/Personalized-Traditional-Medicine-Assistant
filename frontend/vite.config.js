d6669013f9ee4442e5fcb5809f2d1da75151d3ca
Suhana Yaseer <127375037+IT22885432@users.noreply.github.com>
2026-01-02 21:12:03 +0530

    add some changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,      // <-- set the port here
-    open: true,      // optional: automatically opens in browser
+    host: "127.0.0.1",
+  
+    port: 3000, // <-- set the port here
+    open: true, // optional: automatically opens in browser
   },
 });
